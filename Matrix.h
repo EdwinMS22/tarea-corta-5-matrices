daa041805d959d6109ab3bb717e3f4ba744073d3
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-06 12:44:25 -0600

    Add attributes, constructor, and destructor to the Matrix class

--- Matrix.h
+++ Matrix.h
@@ -1,3 +1,27 @@
 #pragma once
+
+#include <iostream>
+#include <stdexcept>
+
+template <typename E>
 class Matrix {
+private:
+	E** matrix;
+	int rows;
+	int cols;
+public:
+	Matrix(int rows, int cols) {
+		if (rows <= 0 || cols <= 0)
+			throw std::runtime_error("Number of rows and columns must be greater than zero.");
+		this->rows = rows;
+		this->cols = cols;
+		matrix = new E*[rows];
+		for (int i = 0; i < rows; i++)
+			matrix[i] = new E[cols];
+	}
+	~Matrix() {
+		for (int i = 0; i < rows; i++)
+			delete[] matrix[i];
+		delete[] matrix;
+	}
 };
