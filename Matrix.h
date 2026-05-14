788af1368b22c95cd34b6d1a87e27dc27c53b5e7
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-14 17:57:37 -0600

    implement Matrix addColumn method

--- Matrix.h
+++ Matrix.h
@@ -85,7 +85,16 @@ public:
 		rows++;
 	}
 	void addColumn(E value) {
-		
+		for (int r = 0; r < rows; r++) {
+			E* temp = new E[cols + 1];
+			for (int c = 0; c < cols; c++) {
+				temp[c] = matrix[r][c];
+			}
+			temp[cols] = value;
+			delete[] matrix[r];
+			matrix[r] = temp;
+		}
+		cols++;
 	}
 	void removeRow(int row) {
 
